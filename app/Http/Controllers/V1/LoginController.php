b1f7a6900c57addb57e5b96df5d35c9c0ed6f116
Mr Plus <duong.pv@neo-lab.vn>
2017-10-04 11:46:40 +0700

    reformat code

--- app/Http/Controllers/V1/LoginController.php
+++ app/Http/Controllers/V1/LoginController.php
@@ -9,32 +9,32 @@ use JWTAuth;
 
 class LoginController extends ApiController
 {
-    /**
-     * Login
-     *
-     * @param Request $request
-     * @return mixed
-     */
-    public function login(Request $request){
-        try {
-            $credential = $request->only(['email' , 'password']);
-            $rules = [
-                'email'     => 'required|email',
-                'password'  => 'required'
-            ];
-            $messages = [];
-            $validator = Validator::make($credential, $rules, $messages);
-            if($validator->fails()){
-                return response()->error($validator->errors());
-            }
-            if(!$token = JWTAuth::attempt($credential)){
-                return response()->error('Email or password is wrong', 400);
-            }
-
-            return response()->success($token);
-        }
-        catch (\Exception $exception){
-            return response()->error('This api error', 500);
-        }
-    }
+	/**
+	 * Login
+	 *
+	 * @param Request $request
+	 * @return mixed
+	 */
+	public function login(Request $request)
+	{
+		try {
+			$credential = $request->only(['email', 'password']);
+			$rules = [
+				'email'    => 'required|email',
+				'password' => 'required',
+			];
+			$messages = [];
+			$validator = Validator::make($credential, $rules, $messages);
+			if ($validator->fails()) {
+				return response()->error($validator->errors());
+			}
+			if (!$token = JWTAuth::attempt($credential)) {
+				return response()->error('Email or password is wrong', 400);
+			}
+			
+			return response()->success($token);
+		} catch (\Exception $exception) {
+			return response()->error('This api error', 500);
+		}
+	}
 }
